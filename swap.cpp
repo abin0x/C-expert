516ad1930532c4ae90859948abd0b18e5790c5a1
abin0x <mahmudulabin@gmail.com>
2025-01-10 19:27:33 +0600

    with pointer sawp

--- swap.cpp
+++ swap.cpp
@@ -1,12 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
+void swap(int *a, int *b)
+{
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
 int main() 
 {
     int a,b;
     cin>>a>>b;
-    int temp=a;
-    a=b;
-    b=temp;
+    // int temp=a;
+    // a=b;
+    // b=temp;
+    swap(&a,&b);
 
     cout<<a<<" "<<b<<endl;
     
