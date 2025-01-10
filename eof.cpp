a0a8ccda3c1de874b1d5d304d33a5e78702c4bad
abin0x <mahmudulabin@gmail.com>
2025-01-10 12:39:48 +0600

    first commit

---
+++ eof.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    int a, b;
+    
+    // Continuously read input until EOF
+    while (cin >> a >> b) 
+    {
+        cout << a << b << endl;
+    }
+    
+    return 0;
+}
