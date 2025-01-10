ead7f618057dba0353a22f045ebf2046142df6db
abin0x <mahmudulabin@gmail.com>
2025-01-10 19:18:54 +0600

    sawp

--- min_max_swap.cpp
+++ min_max_swap.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 int main() 
 {
-    int a,b;
-    cin>>a>>b;
-    cout<<"Before swapping: "<<a<<" "<<b<<endl;
-    int c=min(a,b);
-    int d=max(a,b);
-    // cout<<"After swapping: "<<d<<" "<<c<<endl;
-    cout<<"After swapping: "<<max(a,b)<<" "<<min(a,b)<<endl;
+    int a,b,e,f;
+    cin>>a>>b>>e>>f;
+    cout<<"Before swapping: "<<a<<" "<<b<<" "<<e<<" "<<f<<endl;
+    int mn=min({a,b,e,f});
+    int mx=max({a,b,e,f});
+    swap(mn,mx);
+    cout<<"After swapping: "<<mn<<" "<<mx<<endl;
     
     return 0;
 }
