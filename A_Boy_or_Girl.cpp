f32747c207388ba8780fe481ef881e439fc79a30
abin0x <mahmudulabin@gmail.com>
2025-01-11 16:01:03 +0600

    Codeforces problem solve

---
+++ A_Boy_or_Girl.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    string s;
+    cin>>s;
+    int count=0;
+    for(int i=0;i<s.size();i++)
+    {
+        for(int j=i+1;j<s.size();j++)
+        {
+            if(s[i]==s[j])
+            {
+                count++;
+                break;
+            }
+        }
+
+    }
+    if((s.size()-count)%2==0)
+    {
+        cout<<"CHAT WITH HER!"<<endl;
+    }
+    else
+    {
+        cout<<"IGNORE HIM!"<<endl;
+    }
+    
+    return 0;
+}
